93348184157bf150adcd2a88f9bde95720060f5f
Hayyuu <hayatahimed518@gmail.com>
2023-06-05 19:48:42 +0300

    Add change box color to random color functionality

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,12 @@ function generateGrid(number){
             box.classList.add('box');
             box.style.cssText=`width:${(320/number)}px;height:${(320/number)}px;`;
             innerConatiner.appendChild(box);
-            box.addEventListener('mouseover',changeColor);
+            if(statusDefault){
+                box.addEventListener('mouseover',changeColor);
+            }
+            else{
+                box.addEventListener('mouseover',changeToRandomColor);
+            }
         }
     }
     }
@@ -17,8 +22,23 @@ function generateGrid(number){
 function changeColor(e){
     e.target.setAttribute('id','changeColor');
 }
+function changeToRandomColor(e){
+    const box=e.target;
+    let rgb1,rgb2,rgb3;
+    rgb1=((Math.random()*1000 +25)/4);
+    rgb2=((Math.random()*1000 +25)/4);
+    rgb3=((Math.random()*1000 +25)/4);
+    box.style.backgroundColor=`rgb(${rgb1},${rgb2},${rgb3})`
+    
+    // box.removeAttribute('id');
+    // console.log(box);
+    // console.log(box.style.backgroundColor);
+    // box.addEventListener('mouseover',function(){
+    // box.style.background=`rgb(${rgb1},${rgb2},${rgb3})`;
+    // });
+}
+let statusDefault=true;
 generateGrid(16);
-let no=Array.from(document.querySelectorAll('.box'));
 let noSquers;
 const container=document.querySelector('.container');
 const noSquersBtn=document.querySelector('#noSquers');
@@ -36,5 +56,11 @@ noSquersBtn.addEventListener('click',function(){
         generateGrid(noSquers);
     }
 });
-
+const randomColor=document.querySelector('#rColor');
+randomColor.addEventListener('click',function(){
+    statusDefault=false;
+    container.textContent="";
+    let size=noSquers?noSquers:16;
+    generateGrid(size);
+});
 
